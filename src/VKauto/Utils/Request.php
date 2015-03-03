6961a6349ab8f1504b11eb83777f6f29201e0623
Miserable Coder <miserable.coder@gmail.com>
2015-03-03 14:51:21 +0300

    Added post() method to Request class

--- src/VKauto/Utils/Request.php
+++ src/VKauto/Utils/Request.php
@@ -12,12 +12,32 @@ class Request
 			CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0',
 			CURLOPT_URL => $url,
 			CURLOPT_RETURNTRANSFER => true,
-			// CURLOPT_HEADER => true,
 			CURLOPT_SSL_VERIFYPEER => false,
 			CURLOPT_SSL_VERIFYHOST => false,
 			CURLOPT_CONNECTTIMEOUT => 0,
 			CURLOPT_TIMEOUT => 400,
 			CURLOPT_POST => false
+		];
+
+		$ch = curl_init();
+		curl_setopt_array($ch, $options);
+		$response = curl_exec($ch);
+
+		return $response;
+	}
+
+	public static function post($url, $data = array())
+	{
+		$options = [
+			CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0',
+			CURLOPT_URL => $url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_SSL_VERIFYHOST => false,
+			CURLOPT_CONNECTTIMEOUT => 0,
+			CURLOPT_TIMEOUT => 400,
+			CURLOPT_POST => true,
+			CURLOPT_POSTFIELDS => $data
 		];
 
 		$ch = curl_init();
