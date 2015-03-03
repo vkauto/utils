2df1848e586c163a3c4f65a83e6ede8e126308db
Miserable Coder <miserable.coder@gmail.com>
2015-03-03 13:33:28 +0300

    Added log class

---
+++ src/VKauto/Utils/Log.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace VKauto\Utils;
+
+class Log
+{
+	public static function write($data, $prefixes = array())
+	{
+		$text = @date("[H:i:s d.m.Y]\x20");
+
+		foreach ($prefixes as $prefix)
+		{
+			$text .= "[{$prefix}]\x20";
+		}
+
+		$text .= $data;
+
+		echo($text . PHP_EOL);
+	}
+}
